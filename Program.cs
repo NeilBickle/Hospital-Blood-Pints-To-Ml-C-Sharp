993aeee514534d78f91f86e6e6b16aaa16e76f2f
Neil Bickle (AMBCS) (AssocIScT) (TMIET) (ICTTech) <43986316+NeilBickle@users.noreply.github.com>
2019-06-09 10:03:50 +0100

    Ver 1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pints_To_ML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            int b;
+            double ans;
+            Console.WriteLine("Enter A Number:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter 1 For Pints To ML Or 2 For ML To Pints");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (b == 1)
+            {
+                ans = a * 473.176;
+                Console.WriteLine("The Answer Is {0}", ans);
+            }
+            else if (b == 2)
+            {
+                ans = a / 473.176;
+                Console.WriteLine("The Answer Is {0}", ans);
+            }
+            Console.ReadLine();
+        }
+    }
+}
